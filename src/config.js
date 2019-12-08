e756bddc4fda37197d8176296aec6ee651f9ed09
Andre Carlos <contato@andrecarlos.com.br>
2019-12-08 15:20:00 -0200

    Versão sem sendgridKey e sem ConnectionString

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://andre:andre@cluster0-rctbw.azure.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridKey: '',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodstr;AccountKey=Ujit8rqj1pUiOUc54C7QUq2SxzoXk2kgn1DtQoCl0pZ/GqB1NDSPmcCGgkvIz84uylHwoEPMrH8HjGCdcJW4ag==;EndpointSuffix=core.windows.net'
+    sendgridKey: 'MySendgridKey',
+    containerConnectionString: 'MyConnectionString'
 }
